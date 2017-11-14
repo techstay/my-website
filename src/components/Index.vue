9332fdd013abfde48b79ffc3c8957c43cc9876d0
yitian <lovery521@gmail.com>
2017-11-15 02:50:07 +0800

    将文件名修改为英文。

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container>
     <b-jumbotron header="欧根欧根欧">
-      <img class="img-thumbnail" src="../assets/img/欧根.png" alt="欧根">
+      <img class="img-thumbnail" src="../assets/img/eugen.png" alt="欧根">
     </b-jumbotron>
   </b-container>
 </template>
