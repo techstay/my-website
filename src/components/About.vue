97b63eec3ae351739f1f0af0acf74e967f953371
yitian <lovery521@gmail.com>
2017-12-12 20:09:50 +0800

    添加了更多联系方式

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,7 @@
             header-bg-variant="primary"
             header-text-variant="white">
       <p class="card-text">
-        这是我为了测试Dedicenter VPS做的测试页面。
+        这是一个使用Vue制作的网站。
       </p>
     </b-card>
 
@@ -22,6 +22,12 @@
         电子邮件：<a href="mailto:lovery521@gmail.com">lovery521@gmail.com</a>
         <br>
         新浪微博：<a href="http://weibo.com/techstay">你们都是技术宅</a>
+        <br>
+        简书：<a href="http://www.jianshu.com/u/7753478e1554">乐百川</a>
+        <br>
+        Github：<a href="https://github.com/techstay">techstay</a>
+        <br>
+        CSDN博客：<a href="http://blog.csdn.net/u011054333?ref=toolbar">过了即是客</a>
       </p>
     </b-card>
   </b-container>
