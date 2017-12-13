53f7e7496e1d0ed14e5eee33930b92a03b539981
yitian <lovery521@gmail.com>
2017-12-14 03:47:08 +0800

    添加了主页图片

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,8 +1,23 @@
 <template>
   <b-container>
-    <b-jumbotron header="欧根欧根欧">
-      <img class="img-thumbnail" src="../assets/img/eugen.png" alt="欧根">
+    <b-jumbotron header="主页我不知道该放啥">
+      <b-carousel controls
+                  indicators
+                  background="#ababab"
+                  :interval="4000"
+                  v-model="slide"
+                  @sliding-start="onSlideStart"
+                  @sliding-end="onSlideEnd"
+      >
+        <b-carousel-slide
+          img-src="http://default-1251266667.file.myqcloud.com/images/LoL-Ero-League-of-Legends-DVa.png"></b-carousel-slide>
+        <b-carousel-slide img-src="http://default-1251266667.file.myqcloud.com/images/eugen.png"></b-carousel-slide>
+        <b-carousel-slide
+          img-src="http://default-1251266667.file.myqcloud.com/images/%E7%81%B5%E6%A2%A6.png"></b-carousel-slide>
+
+      </b-carousel>
     </b-jumbotron>
+
   </b-container>
 </template>
 
